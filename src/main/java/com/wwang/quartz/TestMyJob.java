5225fd66d2ef3d4d9749b2f43d8f931f53a2ffcb
williamwangwei <2840769859@qq.com>
2017-04-24 15:58:59 +0800

    提交测试学习用例

--- src/main/java/com/wwang/quartz/TestMyJob.java
+++ src/main/java/com/wwang/quartz/TestMyJob.java
@@ -7,12 +7,15 @@ public class TestMyJob {
     public static String TRIGGER_GROUP_NAME = "XLXXCC_JOB_GROUP"; 
 
     public static void main(String[] args) {
+    	
         try {  
             System.out.println("【系统启动】开始(每1秒输出一次)...");    
             QuartzManager.addJob(JOB_NAME, JOB_GROUP_NAME, TRIGGER_NAME, TRIGGER_GROUP_NAME, MyJob.class, "0/1 * * * * ?");    
 
-            Thread.sleep(3000);    
-            System.out.println("【修改时间】开始(每5秒输出一次)...");    
+            QuartzManager.joblist();
+            Thread.sleep(3000);  
+            
+            /*System.out.println("【修改时间】开始(每5秒输出一次)...");    
             QuartzManager.modifyJobTime(JOB_NAME, JOB_GROUP_NAME, TRIGGER_NAME, TRIGGER_GROUP_NAME, "0/5 * * * * ?");    
 
             Thread.sleep(6000);    
@@ -27,9 +30,11 @@ public class TestMyJob {
             Thread.sleep(6000);    
             System.out.println("【移除定时】开始...");    
             QuartzManager.removeJob(JOB_NAME, JOB_GROUP_NAME, TRIGGER_NAME, TRIGGER_GROUP_NAME);    
-            System.out.println("【移除定时】成功"); 
+            System.out.println("【移除定时】成功"); */
         } catch (Exception e) {  
             e.printStackTrace();  
         }  
+        
+        
     }
 }
