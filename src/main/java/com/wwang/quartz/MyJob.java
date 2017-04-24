406208abcda0bc908ecc89ed34ced3f97c950e82
williamwangwei <2840769859@qq.com>
2017-04-24 15:59:40 +0800

    提交

--- src/main/java/com/wwang/quartz/MyJob.java
+++ src/main/java/com/wwang/quartz/MyJob.java
@@ -2,15 +2,24 @@ package com.wwang.quartz;
 
 import java.util.Date;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class MyJob implements Job {
 
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
-		System.out.println(new Date() + ": doing something...");
+		System.out.println(Thread.currentThread().getName()+""+new Date() + ": doing something...");
+		
+		try {
+			Thread.sleep(1000 * 5);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
